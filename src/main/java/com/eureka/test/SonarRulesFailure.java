78d83caf087f2075cf95855e5fddab0f25ac3167
SrikanthK <srikanthkoyyada@gmail.com>
2020-04-20 17:34:03 +0530

    test
    Signed-off-by: SrikanthK <srikanthkoyyada@gmail.com>

--- src/main/java/com/eureka/test/SonarRulesFailure.java
+++ src/main/java/com/eureka/test/SonarRulesFailure.java
@@ -1,40 +1,8 @@
 package com.eureka.test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class SonarRulesFailure {
 
 	StringBuffer buffer = new StringBuffer("Srikanth");
-	//String password="srikanth";
-	
-	public void getConnection() {
-	
-	Connection conn = null;
-	try {
-	  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-	        "user=steve&password=blue"); 
-	  
-	  String uname = getEncryptedUser();
-	  String password = getEncryptedPassword();
-	  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-	        "user=" + uname + "&password=" + password); 
-
-	  java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  
-	  
-	}
-	catch(SQLException exception) {
-		System.out.println(exception.getMessage());
-	}
-	}
-	
-	private String getEncryptedUser() {
-		return "user";
-	}
-	
-	private String getEncryptedPassword() {
-		return "password";
-	}
+	// String password="srikanth";
 
 }
